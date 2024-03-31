72e2be7490422102445e9090511f87c0555371fa
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-01 00:04:43 +0200

    updated

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    nums = dict
+    nums = {}
     sum = 0
     for num in my_list:
-        nums[num] += 1
+        nums[num] = 1
     for item in nums:
         sum += item
     return sum
