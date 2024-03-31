eb7e28a9dc471c6971cd84b78a503112966e1203
Maysaa55 <maysaaalimhmd@gmail.com>
2024-03-31 23:42:39 +0200

    file1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list1 = []
+    fir num in my_list:
+        if num == search:
+            list1.append(replace)
+        else:
+            list1.append(num)
+return list1
