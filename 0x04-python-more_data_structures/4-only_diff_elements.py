afe6caec5c182974ff69c3a3a029decebb4ad1a8
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-01 00:15:11 +0200

    file4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    list1 = []
+    for item in set_1:
+        if item not in set_2:
+            list1.append(item)
+    for item in set_2:
+        if item not in set_1:
+            list1.apend(item)
+    return list1
