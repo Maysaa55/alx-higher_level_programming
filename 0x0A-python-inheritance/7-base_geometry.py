b649efe342f1da8136f44e03bce56405d407e920
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2024-04-16 00:22:24 +0200

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,6 +19,8 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
+        if value == None and name == None:
+            raise ValueError("integer_validator missing 2 arguements")
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
