99fede06e14c1c76ac441a30e8c88f6f692caead
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-16 23:11:37 +0200

    file0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defines a function reads a file and print it."""
+
+def read_file(filename=""):
+    """representing the function.
+    Args:
+    filename (any): the name of the file to be read;
+    """
+    with open(filename, encoding="utf-8") as fl:
+        print(fl.read(), end = "")
