ce8caa669a7c204f91d7f4ca568da9247191b68a
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-16 23:41:15 +0200

    update

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Define a function returns the json of an object."""
+
+
+import json
+def to_json_string(my_obj):
+    """represents the function.
+    Args:
+    my_obj (any): the onject to convert to json.
+    Returns:
+    the json representation to the object.
+    """
+
+    return json.dumps(my_obj)
