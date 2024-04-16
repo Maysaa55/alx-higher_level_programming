9c5bc2841de99e527a4e87c8e7340c5318dcecd4
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-17 00:00:37 +0200

    create

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a function write json string to a text file."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function's representation.
+    Args:
+    my_obj (any): the object to convert.
+    filename (any): the file to write the json representation to .
+    Returns:
+    nothing.
+    """
+
+    with open(filename, mode= 'w', encoding= "utf-8") as fl:
+        fl.write(json.dumps(my_obj))
