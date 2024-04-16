3e3b33bf527360f11562aa248d8e53d500afefd3
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-16 23:18:36 +0200

    created

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines function that wrie to a file"""
+
+
+def write_file(filename="", text=""):
+    """ represent the function.
+    Args:
+    filename (any): the name of the file to write in .
+    text (any): the text to write in the file.
+    Returns:
+    the number of characters added to the file.
+    """
+    with open(filename, mode= 'w', encoding= "utf-8") as fl:
+        num = fl.write(text)
+        return num
