16abf481a6423610f024ae28e52da07e8eeb8033
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-16 16:38:19 -0700

    dbsdm

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,11 +7,13 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """ the constructor method."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     def to_json(self, attrs=None):
         """ the attributes method."""
+
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
