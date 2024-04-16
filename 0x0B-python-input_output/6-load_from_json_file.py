b0cd663d72f6860b3d22bbd0025fdfe0c0cec46d
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-17 00:06:05 +0200

    created

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define a function that creates an object from json file."""
+import json
+
+
+def load_from_json_file(filename):
+    """ represent the function.
+    Args:
+    filename (any): the json file.
+    Returns:
+    nothing.
+    """ 
+
+    with open(filename, encoding= "utf-8") as fl:
+        oobj = json.loads(fl.read())
