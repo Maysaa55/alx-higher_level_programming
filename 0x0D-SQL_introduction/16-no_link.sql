8d96a16f606b227446fc7dcd17530cd640386536
Maysaa55 <maysaaalimhmd@gmail.com>
2024-05-24 23:57:24 +0200

    w

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- list all records in table.
-SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
 
